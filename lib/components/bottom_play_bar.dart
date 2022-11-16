4c44785909c176a2107e4c0c92507f6a743b9cb1
yjjo8136 <yjun8136@gmail.com>
2022-11-16 13:24:21 +0900

    home page design 완료

--- lib/components/bottom_play_bar.dart
+++ lib/components/bottom_play_bar.dart
@@ -9,35 +9,39 @@ class BottomPlayBar extends StatelessWidget {
       color: Color(0xCC7C001A),
       child: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '민족의 아리아',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  '고려대학교',
-                  style: TextStyle(
-                    color: Color(0x80FFFFFF),
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-          ),
+          _buildPlayingSongInfo(),
           Spacer(),
           Icon(Icons.skip_previous, size: 40),
           Icon(Icons.play_arrow, size: 60),
           Icon(Icons.skip_next, size: 40),
           SizedBox(width: 20),
           Icon(Icons.playlist_play, size: 50),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPlayingSongInfo() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '민족의 아리아',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '고려대학교',
+            style: TextStyle(
+              color: Color(0x80FFFFFF),
+              fontSize: 16,
+            ),
+          ),
         ],
       ),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,90 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Main Page')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8, bottom: 16),
+              child: Container(
+                width: double.infinity,
+                child: Text(
+                  '인기 순위',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  _buildSongTile(),
+                  _buildSongTile(),
+                  _buildSongTile(),
+                  _buildSongTile(),
+                  _buildSongTile(),
+                  _buildSongTile(),
+                  _buildSongTile(),
+                  _buildSongTile(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSongTile() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16, bottom: 16),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Color(0xFFDBBA81),
+        ),
+        width: double.infinity,
+        height: 80,
+        child: Row(
+          children: [
+            SizedBox(width: 25),
+            Image.asset('assets/korea_univ_logo.png', height: 45, width: 45),
+            SizedBox(width: 10),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '1    민족의 아리아',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    '          고려대학교',
+                    style: TextStyle(
+                      color: Color(0x4D000000),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
+            Icon(Icons.play_arrow, size: 30),
+            Icon(Icons.more_vert, size: 30),
+            SizedBox(width: 10),
+          ],
+        ),
+      ),
     );
   }
 }
