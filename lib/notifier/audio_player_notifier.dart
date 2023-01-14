9bcbc75b799afc7330bfdc2b36dcf4a52ac77f7d
yjjo8136 <yjun8136@gmail.com>
2023-01-15 01:57:11 +0900

    fix: 플레이리스트 편집 후 sharedPreference에 저장되도록 수정

--- lib/notifier/audio_player_notifier.dart
+++ lib/notifier/audio_player_notifier.dart
@@ -192,6 +192,8 @@ class AudioPlayerNotifier extends ChangeNotifier {
     final element = _playlist.removeAt(oldIndex);
     _playlist.insert(newIndex, element);
     audioPlayerPlaylist.move(oldIndex, newIndex);
+    print("!@#!@#!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@!@");
+    _saveToPrefs();
     notifyListeners();
   }
 
